7e59082f346c3d37896f2c7f5b8babee49624c13
juliovalenciag <valenciagranadaj@gmail.com>
2024-04-21 10:19:43 -0600

    modulos con frames

---
+++ determinants.py
@@ -0,0 +1,160 @@
+from fractions import Fraction
+
+from PIL import ImageTk, Image
+import tkinter as tk
+from tkinter import ttk
+import tkinter.messagebox
+import customtkinter
+import os
+
+
+class DeterminantsFrame(customtkinter.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.configure_mainContent()
+
+        self.configure_topbar()
+
+        self.cell_size = 50
+        self.cell_padding = 10
+
+    def configure_topbar(self):
+        topbar_frame = customtkinter.CTkFrame(self, height=100, corner_radius=0)
+        topbar_frame.grid(row=0, column=1, columnspan=3, sticky="nsew")
+
+        button_info = [("importar.png", "Importar", self.toolbar_button_click),
+                       ("exportar.png", "Exportar", self.toolbar_button_click),
+                       ("resolver.png", "Resolver", self.toolbar_button_click),
+                       ("inversa.png", "Inversa", self.toolbar_button_click),
+                       ("limpiar.png", "Reiniciar",self.toolbar_button_click ),
+                       ("Cambiar.png", "Seleccionar tamaño", self.matrix_size), ]
+
+        for i, (img_name, text, cmd) in enumerate(button_info):
+            try:
+                image_path = os.path.join("images", img_name)
+                pil_image = Image.open(image_path).resize((50, 50))
+                tk_image = ImageTk.PhotoImage(pil_image)
+                button = customtkinter.CTkButton(topbar_frame, image=tk_image, text=text, command=cmd,
+                                                 compound="top",
+                                                 fg_color=None, hover_color="gray")
+                button.image = tk_image
+                button.grid(row=0, column=i, padx=10, pady=10)
+            except FileNotFoundError:
+                print(f"Error: El archivo {img_name} no se encontró en la carpeta 'images'.")
+
+    def toolbar_button_click(self):
+        print("Toolbar button clicked")
+
+    def configure_mainContent(self):
+        self.mainEntry_frame = customtkinter.CTkFrame(self)
+        self.mainEntry_frame.grid(row=1, column=1, rowspan=3, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.grid_columnconfigure(1, weight=1)
+
+        self.create_matrix_entries(3)
+
+        self.result_label = customtkinter.CTkLabel(self.mainEntry_frame, text="Determinante:", font=('Arial', 14))
+        self.result_label.grid(row=1, column=1, padx=10, pady=10)
+
+        self.mainSolution_frame = customtkinter.CTkFrame(self)
+        self.mainSolution_frame.grid(row=1, column=2, rowspan=2, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        self.grid_columnconfigure(2, weight=1)
+
+        self.mainResults_frame = customtkinter.CTkFrame(self)
+        self.mainResults_frame.grid(row=3, column=2, padx=(20, 20), pady=(20, 0), sticky="nsew")
+
+        self.grid_rowconfigure(3, weight=1)
+
+        self.clear_content_button = customtkinter.CTkButton(self, text="Limpiar contenido", fg_color="transparent",
+                                                            border_width=1, text_color=("gray10", "#DCE4EE"),
+                                                            command=self.clear_matrix_content)
+        self.clear_content_button.grid(row=4, column=1, padx=(20, 0), pady=(20, 20), sticky="nsew")
+
+        self.label_determinant = customtkinter.CTkLabel(self, text="Determinante de matriz")
+        self.label_determinant.grid(row=4, column=2, padx=(20, 20), pady=(20, 20), sticky="nsew")
+
+    #A  partir de aqui va lo de formar la matriz:
+    def matrix_size(self):
+        self.matrix_window = customtkinter.CTkToplevel(self)
+        self.matrix_window.title("Adjust Matrix Size")
+        self.matrix_window.geometry("600x500")
+
+        self.size = 3
+        self.canvas = tk.Canvas(self.matrix_window, width=500, height=400)
+        self.canvas.pack(pady=20, padx=20)
+        self.canvas.bind("<B1-Motion>", self.resize_matrix)
+        self.draw_matrix(self.size)
+
+
+    def draw_matrix(self, size):
+        self.canvas.delete("all")
+        start_x = start_y = 50
+        end_x = end_y = start_x + size * (self.cell_size + self.cell_padding) - self.cell_padding
+
+        for i in range(size):
+            for j in range(size):
+                x1 = start_x + j * (self.cell_size + self.cell_padding)
+                y1 = start_y + i * (self.cell_size + self.cell_padding)
+                x2 = x1 + self.cell_size
+                y2 = y1 + self.cell_size
+                self.canvas.create_rectangle(x1, y1, x2, y2, outline="black", fill="lightgrey")
+    def resize_matrix(self, event):
+        size = int((max(event.x, event.y) - 50) / (self.cell_size + self.cell_padding)) + 1
+        if size != self.size and 1 <= size <= 10:
+            self.size = size
+            self.draw_matrix(self.size)
+
+    def accept_size(self):
+        self.create_matrix_entries(self.size)
+        self.matrix_window.destroy()
+
+    def create_matrix_entries(self, size):
+        for widget in self.mainEntry_frame.winfo_children():
+            widget.destroy()
+
+        self.matrix_entries = []
+        entry_width = 60
+        entry_height = 60
+        padding = 5
+        bg_color_default = "#2C2F33" if customtkinter.get_appearance_mode() == "Dark" else "white"
+
+        total_width = size * (entry_width + padding)
+        total_height = size * (entry_height + padding)
+
+        start_x = (self.mainEntry_frame.winfo_width() - total_width) // 2
+        start_y = (self.mainEntry_frame.winfo_height() - total_height) // 2
+
+        for i in range(size):
+            row_entries = []
+            for j in range(size):
+                entry = customtkinter.CTkEntry(self.mainEntry_frame, width=entry_width, height=entry_height,
+                                               corner_radius=5, fg_color=bg_color_default)
+                entry.place(x=start_x + j * (entry_width + padding), y=start_y + i * (entry_height + padding))
+                row_entries.append(entry)
+            self.matrix_entries.append(row_entries)
+
+    def update_rows_columns(self, event=None):
+        self.rows = int(self.row_box.get())
+        self.columns = int(self.col_box.get())
+        self.draw_matrix()
+        self.create_matrix_entries(self.rows, self.columns)
+
+    def create_rounded_rectangle(self, x1, y1, x2, y2, radius=25, **kwargs):
+        points = [x1+radius, y1,
+                  x1+radius, y1, x2-radius, y1, x2-radius, y1, x2, y1,
+                  x2, y1, x2, y1+radius, x2, y1+radius, x2, y2-radius,
+                  x2, y2-radius, x2, y2, x2-radius, y2, x2-radius, y2,
+                  x1+radius, y2, x1+radius, y2, x1, y2, x1, y2-radius,
+                  x1, y2-radius, x1, y1+radius, x1, y1+radius, x1, y1]
+        return self.canvas.create_polygon(points, **kwargs, smooth=True)
+
+    def clear_matrix_content(self):
+        self.create_matrix_entries(3)
+
+
+
+
+if __name__ == "__main__":
+    root = customtkinter.CTk()
+    gj_frame = DeterminantsFrame(root)
+    gj_frame.pack(fill="both", expand=True)
+    root.mainloop()
--- gauss_jordan.py
+++ gauss_jordan.py
@@ -7,25 +7,12 @@ import tkinter.messagebox
 import customtkinter
 import os
 
-customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
-class App(customtkinter.CTk):
-    def __init__(self):
-        super().__init__()
-
-        self.title("Gauss-Jordan")
-        self.geometry(f"{1280}x{720}")
-        self.inverse_tab_created = False
-
-        self.grid_columnconfigure(0, weight=0)
-        self.grid_rowconfigure(0, weight=0)
-        self.grid_columnconfigure((1, 2), weight=1)
-        self.grid_rowconfigure((1, 2, 3), weight=1)
-
-        self.configure_topbar()
-        self.configure_sidebar()
+class GaussJordanFrame(customtkinter.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
         self.configure_mainContent()
+        self.configure_topbar()
 
     def configure_topbar(self):
         topbar_frame = customtkinter.CTkFrame(self, height=100, corner_radius=0)
@@ -36,71 +23,43 @@ class App(customtkinter.CTk):
                        ("resolver.png", "Resolver", self.gauss_jordan),
                        ("inversa.png", "Inversa", self.calculate_inverse),
                        ("limpiar.png", "Reiniciar", self.clear_all),
-                       ("Cambiar.png", "Cambiar tamaño", self.matrix_size),]
+                       ("Cambiar.png", "Seleccionar tamaño", self.matrix_size), ]
 
         for i, (img_name, text, cmd) in enumerate(button_info):
             try:
                 image_path = os.path.join("images", img_name)
                 pil_image = Image.open(image_path).resize((50, 50))
                 tk_image = ImageTk.PhotoImage(pil_image)
-                button = customtkinter.CTkButton(topbar_frame, image=tk_image, text=text, command=cmd, compound="top",
+                button = customtkinter.CTkButton(topbar_frame, image=tk_image, text=text, command=cmd,
+                                                 compound="top",
                                                  fg_color=None, hover_color="gray")
                 button.image = tk_image
                 button.grid(row=0, column=i, padx=10, pady=10)
             except FileNotFoundError:
                 print(f"Error: El archivo {img_name} no se encontró en la carpeta 'images'.")
 
-    def configure_sidebar(self):
-        sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
-        sidebar_frame.grid(row=0, column=0, rowspan=6, sticky="nsew")
-        sidebar_frame.grid_rowconfigure(6, weight=1)
-
-        text_label = customtkinter.CTkLabel(sidebar_frame, text="Algebra Lineal",
-                                            font=customtkinter.CTkFont(size=20, weight="bold"))
-        text_label.grid(row=0, column=0, padx=20, pady=(10, 10))
-
-        # Define buttons without executing the method, just pass the reference
-        buttons_info = [
-            ("Gauss-Jordan", self.gauss_jordan),
-            ("Determinantes", self.setup_determinants_view),  # Just pass the method reference
-            ("Suma", self.Suma),
-            ("Multiplicacion", self.Multiplicacion),
-            ("Configuración", self.Configuracion)
-        ]
-
-        for i, (text, command) in enumerate(buttons_info, start=1):
-            button = customtkinter.CTkButton(sidebar_frame, text=text, command=command)
-            button.grid(row=i, column=0, padx=20, pady=10)
-
-        appearance_mode_label = customtkinter.CTkLabel(sidebar_frame, text="Appearance Mode:", anchor="w")
-        appearance_mode_label.grid(row=7, column=0, padx=20, pady=(10, 0))
-        appearance_mode_optionemenu = customtkinter.CTkOptionMenu(sidebar_frame, values=["Light", "Dark", "System"],
-                                                                  command=self.change_appearance_mode_event)
-        appearance_mode_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 10))
-        scaling_label = customtkinter.CTkLabel(sidebar_frame, text="UI Scaling:", anchor="w")
-        scaling_label.grid(row=9, column=0, padx=20, pady=(10, 0))
-        scaling_optionemenu = customtkinter.CTkOptionMenu(sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
-                                                          command=self.change_scaling_event)
-        scaling_optionemenu.grid(row=10, column=0, padx=20, pady=(10, 20))
-        appearance_mode_optionemenu.set("Dark")
-        scaling_optionemenu.set("100%")
-
     def configure_mainContent(self):
         self.mainEntry_frame = customtkinter.CTkFrame(self)
         self.mainEntry_frame.grid(row=1, column=1, rowspan=3, padx=(20, 0), pady=(20, 0), sticky="nsew")
-
-        self.mainResults_frame = customtkinter.CTkFrame(self)
-        self.mainResults_frame.grid(row=1, column=2, rowspan=2, padx=(20,20), pady=(20,0), sticky="nsew")
+        self.grid_columnconfigure(1, weight=1)
 
         self.mainSolution_frame = customtkinter.CTkFrame(self)
-        self.mainSolution_frame.grid(row=3, column=2, padx=(20, 20), pady=(20, 0), sticky="nsew")
-        self.mainSolution_frame.grid_columnconfigure(0, weight=1)
+        self.mainSolution_frame.grid(row=1, column=2, rowspan=2, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        self.grid_columnconfigure(2, weight=1)
 
-        self.clear_content_button = customtkinter.CTkButton(self, text="Limpiar contenido",fg_color="transparent",border_width=1, text_color=("gray10", "#DCE4EE"),command=self.clear_matrix_content)
+        self.mainResults_frame = customtkinter.CTkFrame(self)
+        self.mainResults_frame.grid(row=3, column=2, padx=(20, 20), pady=(20, 0), sticky="nsew")
+
+        self.grid_rowconfigure(3, weight=1)
+
+        self.clear_content_button = customtkinter.CTkButton(self, text="Limpiar contenido", fg_color="transparent",
+                                                            border_width=1, text_color=("gray10", "#DCE4EE"),
+                                                            command=self.clear_matrix_content)
         self.clear_content_button.grid(row=4, column=1, padx=(20, 0), pady=(20, 20), sticky="nsew")
 
-        self.bottomBar_frame_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"))
-        self.bottomBar_frame_button_1.grid(row=4, column=2, padx=(20, 20), pady=(20, 20), sticky="nsew")
+        self.clear_content_button = customtkinter.CTkButton(self, text="Boton", fg_color="transparent",
+                                                            border_width=1, text_color=("gray10", "#DCE4EE"),)
+        self.clear_content_button.grid(row=4, column=2, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
     def create_matrix_entries(self, rows, columns):
         for widget in self.mainEntry_frame.winfo_children():
@@ -111,14 +70,16 @@ class App(customtkinter.CTk):
         entry_width = 60
         entry_height = 60
         padding = 5
-        constant_term_column = columns - 1
+        bracket_width = 20
 
         if customtkinter.get_appearance_mode() == "Dark":
             bg_color_default = "#2C2F33"
             bg_color_constant = "#60656b"
+            bracket_color = "white"
         else:
             bg_color_default = "white"
             bg_color_constant = "lightgrey"
+            bracket_color = "black"
 
         total_width = columns * (entry_width + padding)
         total_height = rows * (entry_height + padding)
@@ -126,15 +87,51 @@ class App(customtkinter.CTk):
         start_x = (self.mainEntry_frame.winfo_width() - total_width) // 2
         start_y = (self.mainEntry_frame.winfo_height() - total_height) // 2
 
+        canvas = tk.Canvas(self.mainEntry_frame, width=self.mainEntry_frame.winfo_width(),
+                           height=self.mainEntry_frame.winfo_height(), highlightthickness=0)
+        canvas.pack(fill='both', expand=True)
+
+        canvas.create_line(start_x - bracket_width, start_y, start_x - bracket_width,
+                           start_y + total_height, width=2, fill=bracket_color)
+        canvas.create_line(start_x + total_width + bracket_width, start_y, start_x + total_width + bracket_width,
+                           start_y + total_height, width=2, fill=bracket_color)
+
         for i in range(rows):
             row_entries = []
             for j in range(columns):
-                bg_color = bg_color_constant if j == constant_term_column else bg_color_default
+                bg_color = bg_color_constant if j == columns - 1 else bg_color_default
                 entry = customtkinter.CTkEntry(self.mainEntry_frame, width=entry_width, height=entry_height,
                                                corner_radius=5, fg_color=bg_color)
+
                 entry.place(x=start_x + j * (entry_width + padding), y=start_y + i * (entry_height + padding))
                 row_entries.append(entry)
             self.matrix_entries.append(row_entries)
+
+    def create_rounded_rectangle(self, x1, y1, x2, y2, radius=25, **kwargs):
+        points = [x1+radius, y1,
+                  x1+radius, y1, x2-radius, y1, x2-radius, y1, x2, y1,
+                  x2, y1, x2, y1+radius, x2, y1+radius, x2, y2-radius,
+                  x2, y2-radius, x2, y2, x2-radius, y2, x2-radius, y2,
+                  x1+radius, y2, x1+radius, y2, x1, y2, x1, y2-radius,
+                  x1, y2-radius, x1, y1+radius, x1, y1+radius, x1, y1]
+        return self.canvas.create_polygon(points, **kwargs, smooth=True)
+
+    def resize_matrix(self, event):
+        start_x = (self.canvas.winfo_width() - self.columns * (self.cell_size + self.cell_padding)) // 2
+        start_y = (self.canvas.winfo_height() - self.rows * (self.cell_size + self.cell_padding)) // 2
+
+        cursor_x = max(self.canvas.canvasx(event.x) - start_x, 0)
+        cursor_y = max(self.canvas.canvasy(event.y) - start_y, 0)
+
+        new_columns = max(int(cursor_x / (self.cell_size + self.cell_padding)) + 1, 1)
+        new_rows = max(int(cursor_y / (self.cell_size + self.cell_padding)) + 1, 1)
+
+        if new_columns != self.columns or new_rows != self.rows:
+            self.columns = min(new_columns, 10)
+            self.rows = min(new_rows, 10)
+            self.row_box.set(self.rows)
+            self.col_box.set(self.columns)
+            self.draw_matrix()
 
     def gauss_jordan(self):
         rows = len(self.matrix_entries)
@@ -240,6 +237,7 @@ class App(customtkinter.CTk):
 
         label = customtkinter.CTkLabel(self.mainSolution_frame, text=solution_text, anchor="w", justify=tk.LEFT)
         label.grid(sticky="nsew", padx=20, pady=20)
+
     def calculate_inverse(self):
         # Obtener las dimensiones de la matriz de la GUI, excluyendo la columna de términos constantes
         rows = len(self.matrix_entries)
@@ -286,34 +284,13 @@ class App(customtkinter.CTk):
         # Mostrar solo la matriz inversa
         self.display_result_matrix(inverse_matrix)
 
-
-    def Determinantes(self):
-        print("Determinantes")
-    def Suma(self):
-        print("Suma")
-    def Multiplicacion(self):
-        print("Multiplicacion")
-    def Configuracion(self):
-        print("Configuracion")
-    def sidebar_button_event(self):
-        print("Sidebar button clicked")
-    def toolbar_button_click(self):
-        print("Toolbar button clicked")
-    def create_rounded_rectangle(self, x1, y1, x2, y2, radius=25, **kwargs):
-        points = [x1+radius, y1,
-                  x1+radius, y1, x2-radius, y1, x2-radius, y1, x2, y1,
-                  x2, y1, x2, y1+radius, x2, y1+radius, x2, y2-radius,
-                  x2, y2-radius, x2, y2, x2-radius, y2, x2-radius, y2,
-                  x1+radius, y2, x1+radius, y2, x1, y2, x1, y2-radius,
-                  x1, y2-radius, x1, y1+radius, x1, y1+radius, x1, y1]
-        return self.canvas.create_polygon(points, **kwargs, smooth=True)
-
     def matrix_size(self):
         self.matrix_window = customtkinter.CTkToplevel(self)
         self.matrix_window.title("Adjust Matrix Size")
         self.matrix_window.geometry("600x500")
 
-        self.canvas = tk.Canvas(self.matrix_window, bg='white', width=500, height=400)
+        bg_color = "#333333" if customtkinter.get_appearance_mode() == "Dark" else "white"
+        self.canvas = tk.Canvas(self.matrix_window, bg=bg_color, width=500, height=400)
         self.canvas.pack(pady=20, padx=20)
 
         self.rows = 3
@@ -321,14 +298,22 @@ class App(customtkinter.CTk):
         self.cell_size = 50
         self.cell_padding = 10
 
-        self.row_box = ttk.Combobox(self.matrix_window, values=list(range(1, 11)), state="readonly", width=5)
+
+        combobox_style = ttk.Style()
+        combobox_style.theme_use('alt')
+        combobox_style.configure("TCombobox", fieldbackground=bg_color, background=bg_color,
+                                 foreground='black' if customtkinter.get_appearance_mode() == "Light" else 'white')
+
+        self.row_box = ttk.Combobox(self.matrix_window, values=list(range(1, 11)), state="readonly", width=5,
+                                    style="TCombobox")
         self.row_box.set(self.rows)
-        self.row_box.pack(side='left', padx=10)
+        self.row_box.pack(side='left', padx=20, pady=20)
         self.row_box.bind("<<ComboboxSelected>>", self.update_rows_columns)
 
-        self.col_box = ttk.Combobox(self.matrix_window, values=list(range(1, 11)), state="readonly", width=5)
+        self.col_box = ttk.Combobox(self.matrix_window, values=list(range(1, 11)), state="readonly", width=5,
+                                    style="TCombobox")
         self.col_box.set(self.columns)
-        self.col_box.pack(side='left', padx=10)
+        self.col_box.pack(side='left', padx=20, pady=20)
         self.col_box.bind("<<ComboboxSelected>>", self.update_rows_columns)
 
         self.accept_button = customtkinter.CTkButton(self.matrix_window, text="Aceptar", command=self.accept_size)
@@ -352,11 +337,21 @@ class App(customtkinter.CTk):
                 self.create_rounded_rectangle(x1, y1, x2, y2, radius=10, outline="black", fill="lightgrey")
 
         bracket_width = 20
+        start_x = (self.canvas.winfo_width() - self.columns * (self.cell_size + self.cell_padding)) // 2
+        start_y = (self.canvas.winfo_height() - self.rows * (self.cell_size + self.cell_padding)) // 2
+
+        if customtkinter.get_appearance_mode() == "Dark":
+            bracket_color = "white"
+        else:
+            bracket_color = "black"
+
         self.canvas.create_line(start_x - bracket_width, start_y, start_x - bracket_width,
-                                start_y + self.rows * (self.cell_size + self.cell_padding), width=2)
+                                start_y + self.rows * (self.cell_size + self.cell_padding),
+                                width=2, fill=bracket_color)
         self.canvas.create_line(start_x + self.columns * (self.cell_size + self.cell_padding) + bracket_width, start_y,
                                 start_x + self.columns * (self.cell_size + self.cell_padding) + bracket_width,
-                                start_y + self.rows * (self.cell_size + self.cell_padding), width=2)
+                                start_y + self.rows * (self.cell_size + self.cell_padding),
+                                width=2, fill=bracket_color)
 
         self.canvas.create_rectangle(
             start_x + self.columns * (self.cell_size + self.cell_padding) - self.cell_padding,
@@ -365,6 +360,7 @@ class App(customtkinter.CTk):
             start_y + self.rows * (self.cell_size + self.cell_padding),
             fill="red", outline="black", width=2
         )
+
     def resize_matrix(self, event):
         start_x = (self.canvas.winfo_width() - self.columns * (self.cell_size + self.cell_padding)) // 2
         start_y = (self.canvas.winfo_height() - self.rows * (self.cell_size + self.cell_padding)) // 2
@@ -415,15 +411,13 @@ class App(customtkinter.CTk):
         self.draw_matrix()
         self.create_matrix_entries(self.rows, self.columns)
 
-    def change_appearance_mode_event(self, new_appearance_mode: str):
-        customtkinter.set_appearance_mode(new_appearance_mode)
+    def toolbar_button_click(self):
+        print("Toolbar button clicked")
 
-    def change_scaling_event(self, new_scaling: str):
-        customtkinter.set_widget_scaling(int(new_scaling.replace("%", "")) / 100)
 
-    def run(self):
-        self.mainloop()
 
 if __name__ == "__main__":
-    app = App()
-    app.run()
+    root = customtkinter.CTk()
+    gj_frame = GaussJordanFrame(root)
+    gj_frame.pack(fill="both", expand=True)
+    root.mainloop()
